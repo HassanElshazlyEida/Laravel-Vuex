7bfc55004ef5513f9ce91e40f518b89191d1751f
HassanElshazlyEida <hassanaboeata@gmail.com>
2023-08-15 17:47:01 +0300

    💥 NEW: Student Model

---
+++ app/Enums/StudentEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self inActive()
+ * @method static self Active()
+ * 
+ */
+
+class StudentEnum extends Enum {
+    
+}
---
+++ database/migrations/2023_08_15_144256_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\StudentEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("class");
+            $table->string("section");
+            $table->unique("email");
+            $table->enum("status",StudentEnum::toArray())->default(StudentEnum::Active());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
